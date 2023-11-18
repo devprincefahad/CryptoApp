516d7061c0cbeec3ccdc3bd9712ac2b3b9c38a04
devprincefahad <1pfchouhan@gmail.com>
2023-11-19 02:38:09 +0530

    added top bar and back press

--- app/src/main/java/com/example/cryptoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/ui/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.cryptoapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.cryptoapp.R
 import com.example.cryptoapp.ui.coin_detail.CoinDetailScreen
 import com.example.cryptoapp.ui.coin_list.CoinListScreen
 import com.example.cryptoapp.ui.theme.CryptoAppTheme
@@ -17,33 +26,73 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
+                val navController = rememberNavController()
+
+                Scaffold(
+                    topBar = {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentRoute = navBackStackEntry?.destination?.route
+
+                        if (currentRoute != Screen.CoinListScreen.route) {
+                            CustomTopAppBar(
+                                onBackClick = { navController.navigateUp() },
+                                title = "About"
+                            )
+                        } else {
+                            CustomTopAppBar(
+                                onBackClick = { /* do nothing */ },
+                                title = "Coins"
+                            )
+                        }
+                    }
+                ) { contentPadding ->
+                    val modifier = Modifier.padding(contentPadding)
                     NavHost(
+                        modifier = modifier,
                         navController = navController,
                         startDestination = Screen.CoinListScreen.route
                     ) {
                         composable(
                             route = Screen.CoinListScreen.route
-                        ){
+                        ) {
                             CoinListScreen(navController)
                         }
 
                         composable(
                             route = Screen.CoinDetailScreen.route + "/{coinId}"
-                        ){
+                        ) {
                             CoinDetailScreen()
                         }
                     }
                 }
             }
+
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopAppBar(
+    onBackClick: () -> Unit,
+    title: String
+) {
+    TopAppBar(
+        navigationIcon = {
+            if (title == "About") IconButton(onClick = { onBackClick() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = "Back"
+                )
+            }
+        },
+        title = {
+            Text(text = title)
+        }
+    )
 }
